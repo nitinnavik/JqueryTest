8e8535dd12b44212f9695418db8bc26d2127f0b5
nitinnavik <naviknitin143@gmail.com>
2022-11-22 01:27:00 +0530

    changes.

--- index.js
+++ index.js
@@ -11,25 +11,3 @@ function openPage(pageName, elmnt, color) {
   document.getElementById(pageName).style.display = "block";
   elmnt.style.backgroundColor = color;
 }
-
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
-
-function checkPriceMode() {
-  console.log(event.target.value);
-  let value = $(".select-price-mode").val();
-  $(".payment-frequency").fadeOut();
-  if (value === "Price By Recyclables") $(".payment-frequency").fadeIn();
-  $(document).ready(function () {
-    var url = "hotels.json";
-
-    $.getJSON(url, function (data) {
-      $.each(data, function (index, value) {
-        console.log(data);
-        $("#select2").append(
-          '<option value="' + value.name + '">' + value.name + "</option>"
-        );
-      });
-    });
-  });
-}
